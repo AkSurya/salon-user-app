ff919dff9a624d94b74613a73ced04126f15ec35
ishan <ishan984ind@gmail.com>
2026-04-10 22:01:22 +0530

    clean repo: removed old project

--- android/app/src/main/kotlin/com/example/saloon_booking_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.saloon_booking_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/Screens/user_session.dart
+++
@@ -1,7 +0,0 @@
-class UserSession {
-  static String displayName = "User";
-  static String email = "user@gmail.com";
-
-  static String get avatarLetter =>
-      email.isNotEmpty ? email[0].toUpperCase() : "U";
-}
